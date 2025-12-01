642f6ecb3746e9d40bd1b51838b1e6ca5f56f16b
daaman3110 <daaman3110@gmail.com>
2025-12-01 13:04:47 +0530

    Added API Routes

---
+++ app/api/routes.py
@@ -0,0 +1,23 @@
+from fastapi import APIRouter
+from app.core.state import peers, messages
+from app.chat.TCP_Client import send_message
+
+router = APIRouter()
+
+
+@router.get("/peers")
+def get_peers():
+    return peers
+
+
+@router.get("/messages")
+def get_messages():
+    return messages
+
+
+@router.post("/send")
+async def send(data: dict):
+    ip = data["ip"]
+    msg = data["msg"]
+    await send_message(ip, msg)
+    return {"status": "sent"}
