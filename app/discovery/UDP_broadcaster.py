572f924acdbada9836a2571ecf4b704872e0703e
daaman3110 <daaman3110@gmail.com>
2025-12-02 10:38:24 +0530

    Reverted back to simple UDP Broadcast

--- app/discovery/UDP_broadcaster.py
+++ app/discovery/UDP_broadcaster.py
@@ -3,33 +3,15 @@ import socket
 from app.config.config import UDP_PORT, DISCOVERY_MESSAGE, BROADCAST_INTERVAL
 
 
-def get_lan_ip():
-    """Return the IP of the active LAN interface."""
-    try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        # This does NOT send packets; it's a trick to get the LAN interface
-        s.connect(("192.168.0.1", 80))
-        ip = s.getsockname()[0]
-        s.close()
-        return ip
-    except:
-        return "0.0.0.0"
-
-
 async def broadcast_presence():
-    lan_ip = get_lan_ip()
-
-    # Determine broadcast (example -> 192.168.0.255)
-    broadcast_ip = lan_ip.rsplit(".", 1)[0] + ".255"
-
+    # 1: Creating a UDP Socket
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    # 2: Enabling Broadcasting
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 
-    # Bind only to LAN interface
-    sock.bind((lan_ip, 0))
-
-    print(f"[UDP] Broadcasting from {lan_ip} → {broadcast_ip}")
-
+    # Sending the broadcast message every "BROADCAST INTERVAL" seconds
     while True:
-        sock.sendto(DISCOVERY_MESSAGE.encode(), (broadcast_ip, UDP_PORT))
+        # 3: Sending a broadcast message
+        sock.sendto(DISCOVERY_MESSAGE.encode(), ("<broadcast>", UDP_PORT))
         await asyncio.sleep(BROADCAST_INTERVAL)
